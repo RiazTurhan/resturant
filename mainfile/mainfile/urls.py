2818154727bfcae24cfae9e945c64f4bd6e8e0e5
RiazTurhan <turhanuddin19711@gmail.com>
2024-03-28 22:42:50 +0600

    link

--- mainfile/mainfile/urls.py
+++ mainfile/mainfile/urls.py
@@ -19,7 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('home.urls')),
+    path('', include('home.urls')),
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
     path('menu/', include('menu.urls')),
